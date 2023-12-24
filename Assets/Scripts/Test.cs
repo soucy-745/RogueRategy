37239b78c0c038e43b300d9ce694d5042bcaef28
ogata <malineog.1268@gmail.com>
2023-12-24 19:22:02 +0900

    コンフリクトテスト

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start called before the frame update
     void Start()
     {
         
